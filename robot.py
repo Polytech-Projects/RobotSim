075128bb6ee398ba2dde9c852e3f42d377cc73a6
BarbDev <BarbDevelop@gmail.com>
2015-05-13 21:33:40 +0200

    -ajout de 2 variables nécessaire à l'animation du robot -modification de la fonction animation afin d'avoir un déplacement fluide

--- robot.py
+++ robot.py
@@ -3,6 +3,7 @@
 __author__ = 'Tanguy,Romain,William'
 
 from tkinter import PhotoImage
+import time
 
 
 class Robot():
@@ -20,19 +21,21 @@ class Robot():
         self.displayimage = self.image.subsample(2, 2)
         self.display = labyrinthe.laby.create_image(self.coord_x, self.coord_y, anchor='nw', image=self.displayimage)
         self.ref_labyrinthe = labyrinthe
+        self.anim_count = 0
+        self.anim = False
 
     def move(self, direction):
         if direction == 'Left':
             new_coord_x = self.logic_coord_x
-            new_coord_y = self.logic_coord_y - 1 # C Left
+            new_coord_y = self.logic_coord_y - 1
         if direction == 'Up':
-            new_coord_x = self.logic_coord_x - 1 # C Up
+            new_coord_x = self.logic_coord_x - 1
             new_coord_y = self.logic_coord_y
         if direction == 'Right':
             new_coord_x = self.logic_coord_x
             new_coord_y = self.logic_coord_y + 1
         if direction == 'Down':
-            new_coord_x = self.logic_coord_x + 1 # C Down
+            new_coord_x = self.logic_coord_x + 1
             new_coord_y = self.logic_coord_y
 
         if self.ref_labyrinthe.test_case_vide(new_coord_x, new_coord_y):
@@ -43,11 +46,41 @@ class Robot():
             self.animation(direction)
 
     def animation(self, direction):
-        if direction == 'Up':
-            self.ref_labyrinthe.laby.move(self.display, 0, -40)
-        if direction == 'Left':
-            self.ref_labyrinthe.laby.move(self.display, -40, 0)
-        if direction == 'Down':
-            self.ref_labyrinthe.laby.move(self.display, 0, 40)
-        if direction == 'Right':
-            self.ref_labyrinthe.laby.move(self.display, 40, 0)
+        """
+        if self.anim is False and self.anim_count == 0:
+            if direction == 'Up':
+                self.ref_labyrinthe.laby.move(self.display, 0, -4)
+            if direction == 'Left':
+                self.ref_labyrinthe.laby.move(self.display, -4, 0)
+            if direction == 'Down':
+                self.ref_labyrinthe.laby.move(self.display, 0, 4)
+            if direction == 'Right':
+                self.ref_labyrinthe.laby.move(self.display, 4, 0)
+            self.anim = True
+            self.anim_count += 1
+            self.ref_labyrinthe.laby.after(500, self.animation(direction))
+        if self.anim is True and self.anim_count < 10:
+            if direction == 'Up':
+                self.ref_labyrinthe.laby.move(self.display, 0, -4)
+            if direction == 'Left':
+                self.ref_labyrinthe.laby.move(self.display, -4, 0)
+            if direction == 'Down':
+                self.ref_labyrinthe.laby.move(self.display, 0, 4)
+            if direction == 'Right':
+                self.ref_labyrinthe.laby.move(self.display, 4, 0)
+            self.anim_count += 1
+            self.ref_labyrinthe.laby.after(500, self.animation(direction))
+        else:
+            self.anim = False
+            self.anim_count = 0 """
+        for i in range(1, 11):
+            if direction == 'Up':
+                self.ref_labyrinthe.laby.move(self.display, 0, -4)
+            if direction == 'Left':
+                self.ref_labyrinthe.laby.move(self.display, -4, 0)
+            if direction == 'Down':
+                self.ref_labyrinthe.laby.move(self.display, 0, 4)
+            if direction == 'Right':
+                self.ref_labyrinthe.laby.move(self.display, 4, 0)
+            self.ref_labyrinthe.laby.update()
+            time.sleep(0.025)
